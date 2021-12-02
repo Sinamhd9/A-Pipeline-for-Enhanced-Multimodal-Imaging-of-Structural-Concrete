80f154d86bbc5e61b94723f91c03fbdf2466a94b
Sina <mehdinia@pdx.edu>
2021-12-01 16:41:08 -0800

    remove

--- XTFM/readFile.m
+++
@@ -1,8 +0,0 @@
-function data = readFile(filename, nS)
-% Reading the raw csv file and cleaning it
-
-data = -xlsread(filename, ['AJ18:AMU' num2str(nS*30+15)]);
-
-data = reshape(data(~isnan(data)), [nS*28, 1000]);
-
-end
